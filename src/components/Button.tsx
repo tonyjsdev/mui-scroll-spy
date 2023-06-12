eb4ad567fd16e846ef3280830c2c2863316519f0
Antonio Pacheco <antonio_pacheco@outlook.com>
2023-06-12 13:50:41 -0700

    feat(button): added a button with scroll-spy
    Button element from mui now to support click to smooth scroll to element

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React, { CSSProperties } from "react";
+import MuiButton, { ButtonProps as MuiButtonProps } from "@mui/material/Button";
+import { ScrollLink } from "react-scroll";
+
+export interface ButtonProps extends MuiButtonProps {
+  /* style applied when element is reached */
+  activeStyle?: CSSProperties;
+  /* Target to scroll to */
+  to: string;
+  /* Scroll additional px ( like padding ) */
+  offset?: number;
+  /* time of the scroll animation */
+  duration?: number;
+  /* Make Link selected when scroll is at its targets position   */
+  spy?: boolean;
+}
+
+const Button = (props: ButtonProps) => {
+  return <MuiButton {...props} />;
+};
+
+export default ScrollLink(Button);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import Button, { ButtonProps } from "./components/Button";
+export { Element } from "react-scroll";
+
+export { Button, ButtonProps };
